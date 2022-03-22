e69fd308bd69391c48dad652ca56d4165281ebcf
kjh950601@naver.com <milaju@MacBook-Pro.local>
2022-03-23 00:25:21 +0900

    SpringStart

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,26 @@
 package hello.hellospring;
 
 
+//import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource datasource) {
+        this.dataSource = datasource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +30,6 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
     }
-
 }
